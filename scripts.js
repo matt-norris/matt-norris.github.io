3e682cbcac99c7d56161c049efb68b1d5dcca28a
matt <norrism7@msu.edu>
2023-06-02 17:29:08 -0400

    final touches to home page

--- scripts.js
+++ scripts.js
@@ -1,11 +1,4 @@
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth',
-      });
-    });
-  });
+
 
   const menuToggle = document.querySelector('.navbar-toggler');
 const navMenu = document.querySelector('nav ul');
